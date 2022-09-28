cdad52cb284517ca81e0762291ae84f30621167d
uxuisooa <sooa0513@naver.com>
2022-09-28 23:58:45 +0900

    TodoHead 컴포넌트에서 useTodoState 를 사용

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -1,5 +1,5 @@
-import React from "react";
 import styled from "styled-components";
+import { useTodoState } from "../TodoContext";
 
 const TodoHeadBlock = styled.div`
   padding-top: 48px;
@@ -26,6 +26,8 @@ const TodoHeadBlock = styled.div`
 `;
 
 const TodoHead = () => {
+  const todos = useTodoState();
+  console.log(todos);
   return (
     <TodoHeadBlock>
       <h1>2022년 10월 01일</h1>
