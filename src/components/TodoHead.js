17942388e145beaf28902ba2a8617e0a82211181
uxuisooa <sooa0513@naver.com>
2022-09-29 01:11:06 +0900

    코드 정리

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -29,7 +29,7 @@ const TodoHeadBlock = styled.div`
 1. Context 를 만들어주었으니, 이제 Context 와 연동을 하여 기능을 구현하자. 
 Context 에 있는 state 를 받아와서 렌더링을 하고, 필요한 상황에는 특정 액션을 dispatch 하면 된다.
 TodoHead 에서는 done 값이 false 인 항목들의 개수를 화면에 보여준다.
-2. 날짜가 보여지는 부분을 작업해주겠습니다. 
+2. 날짜가 보여지는 부분을 작업하자. 
 이 과정에서는 Date 의 toLocaleString 이라는 함수를 사용합니다.
 */
 const TodoHead = () => {
