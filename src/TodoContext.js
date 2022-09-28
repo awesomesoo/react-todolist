758da4cf3990cfe0b5fd8f809b903b7a4c19690c
uxuisooa <sooa0513@naver.com>
2022-09-28 17:03:32 +0900

    코드 및 설명 정리

--- src/TodoContext.js
+++ src/TodoContext.js
@@ -41,7 +41,16 @@ const todoReducer = (state, action) => {
       throw new Error(`Unhandled action type: ${action.type}`);
   }
 };
+/* 
+Context 만들기 :
 
+이제, state 와 dispatch 를 Context 통하여 다른 컴포넌트에서 바로 사용 할 수 있게 해줄건데요, 
+우리는 하나의 Context 를 만들어서 state 와 dispatch 를 함께 넣어주는 대신에, 
+두개의 Context 를 만들어서 따로 따로 넣어줄 것입니다. 
+
+이렇게 하면 dispatch 만 필요한 컴포넌트에서 불필요한 렌더링을 방지 할 수 있습니다. 
+추가적으로, 사용하게 되는 과정에서 더욱 편리하기도 합니다.
+*/
 const TodoStateContext = createContext();
 const TodoDispatchContext = createContext();
 
@@ -56,15 +65,14 @@ export const TodoProvider = ({ children }) => {
   );
 };
 
+/* 
+커스텀 Hook 만들기:
+컴포넌트에서 useContext 를 직접 사용하는 대신에, 
+useContext 를 사용하는 커스텀 Hook 을 만들어서 내보내주겠습니다.
+*/
 export const useTodoState = () => {
   return useContext(TodoStateContext);
 };
 export const useTodoDispatch = () => {
   return useContext(TodoDispatchContext);
 };
-
-/* 
-커스텀 Hook 만들기:
-컴포넌트에서 useContext 를 직접 사용하는 대신에, 
-useContext 를 사용하는 커스텀 Hook 을 만들어서 내보내주겠습니다.
-*/
