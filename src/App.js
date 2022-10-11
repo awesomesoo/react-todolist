7a92006e6a310ea61a02bf3d96f86af68fb356a0
uiuxsooa <sooa0513@naver.com>
2022-10-12 04:34:19 +0900

    gh-pages과 관련한 내용 추가

--- src/App.js
+++ src/App.js
@@ -12,19 +12,17 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 function App() {
+  // TodoProvider 바깥에서 컨텍스트를 사용하려면 에러가 뜹니다.
+  // 그러면 잘못된 곳에서 함수를 호출했구나를 알 수 있어요.
   return (
-    <>
-      {/* TodoProvider 바깥에서 컨텍스트를 사용하려면 에러가 뜹니다.
-          그러면 잘못된 곳에서 함수를 호출했구나를 알 수 있어요. */}
-      <TodoProvider basename={process.env.PUBLIC_URL}>
-        <GlobalStyle />
-        <TodoTemplate>
-          <TodoHead />
-          <TodoList />
-          <TodoCreate />
-        </TodoTemplate>
-      </TodoProvider>
-    </>
+    <TodoProvider basename={process.env.PUBLIC_URL}>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
+    </TodoProvider>
   );
 }
 
