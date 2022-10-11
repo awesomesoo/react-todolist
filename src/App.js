989709414ec46472980e31d5288029406b123798
uiuxsooa <sooa0513@naver.com>
2022-10-12 04:48:14 +0900

    gh-pages과 관련한 내용 추가

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { createGlobalStyle } from 'styled-components';
 import TodoTemplate from './components/TodoTemplate';
 import TodoHead from './components/TodoHead';
@@ -15,14 +17,16 @@ function App() {
   // TodoProvider 바깥에서 컨텍스트를 사용하려면 에러가 뜹니다.
   // 그러면 잘못된 곳에서 함수를 호출했구나를 알 수 있어요.
   return (
-    <TodoProvider basename={process.env.PUBLIC_URL}>
-      <GlobalStyle />
-      <TodoTemplate>
-        <TodoHead />
-        <TodoList />
-        <TodoCreate />
-      </TodoTemplate>
-    </TodoProvider>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <TodoProvider>
+        <GlobalStyle />
+        <TodoTemplate>
+          <TodoHead />
+          <TodoList />
+          <TodoCreate />
+        </TodoTemplate>
+      </TodoProvider>
+    </BrowserRouter>
   );
 }
 
